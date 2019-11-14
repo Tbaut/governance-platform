2caa15c27f0bf8c03d41dda79ff8d28a73b81875
erler <michelerler@Michels-MBP.parity>
2019-11-14 10:50:57 +0100

    my first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
-import './index.css';
 import App from './App';
+import './index.css';
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_HASURA_GRAPHQL_URL
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { LatestPostsQuery } from '../../generated/graphql';
 import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import * as moment from 'moment';
+
 
 interface Props {
   data: LatestPostsQuery;
@@ -10,20 +14,28 @@ const className = 'Home';
 
 const Home: React.FC<Props> = ({ data }) => (
   <div className={className}>
-    <h3>Latest posts</h3>
-    <ul className={`${className}__list`}>
-      {!!data.posts &&
-        data.posts.map(
-          (post) =>
-            !!post && (
-              <li key={post.id} className={`${className}__item`}>
-                <Link to={`/post/${post.id}`}>
-                  {post.author.username} ({post.creation_date}): {post.title}
-                </Link>
-              </li>
-            ),
-        )}
-    </ul>
+      <h3>Latest posts</h3>
+      <Row>
+        <Col md={6} lg={8}>
+          <ul className={`${className}__list`}>
+            {!!data.posts &&
+              data.posts.map(
+                (post) =>
+                  !!post && (
+                    <li key={post.id} className={`${className}__item`}>
+                      <Link to={`/post/${post.id}`}>
+                        <li style={{color:"#282828", fontWeight: 500, fontSize: "18px", marginBottom: "5px"}}>{post.title}</li>
+                        <li style={{color:"#555", fontSize: "14px"}}>posted by {post.author.username} ({post.creation_date})</li>
+                      </Link>
+                    </li>
+                  ),
+              )}
+          </ul>
+        </Col>
+        <Col>
+          <h5>Hello</h5>
+        </Col>
+      </Row>
   </div>
 );
 
